bda343818c1d9010adc9b93810821156e4e1fb76
qutlawsoasis-debug <qutlawsoasis@gmail.com>
2026-07-28 16:08:33 +0200

    Fix expo-file-system import to legacy API

--- utils/updater.js
+++ utils/updater.js
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as IntentLauncher from 'expo-intent-launcher';
 
 const GITHUB_OWNER = 'qutlawsoasis-debug';
